3585a17c9f0e235c9c4fcf6306efa31a9e321d4b
kana-rus <kanarus786@gmail.com>
2023-05-14 17:37:32 +0900

    save@2023-05-14 17:37+9:00

--- src/__q__/fetch_query.rs
+++ src/__q__/fetch_query.rs
@@ -1,61 +1,107 @@
 use std::{pin::Pin, future::Future};
-use futures_util::{TryStreamExt, StreamExt, FutureExt};
+use futures_util::{TryStreamExt};
 use sqlx::{Executor, Either};
-use crate::{__feature__, Error, Model, FetchAll, FetchOne, FetchOptional, q, pool};
+use crate::{Error, Model, FetchAll, FetchOne, FetchOptional, q, pool};
 use super::param::Param;
 
-/*
+macro_rules! impl_q_fetch_all_with_params {
+    ($( $param:ident )*) => {
+        #[allow(non_camel_case_types)]
+        impl<'q, M:Model+'q, $( $param:Param<'q> ),*> FnOnce<(FetchAll<'q, M>, $( $param ),*)> for q {
+            type Output = Pin<Box<dyn Future<Output = Result<Vec<M>, Error>> + 'q>>;
+            extern "rust-call" fn call_once(self,
+                (FetchAll{sql, __as__}, $( $param ),*): (FetchAll<'q, M>, $( $param ),*)
+            ) -> Self::Output {
+                Box::pin(
+                    Box::pin(
+                        pool().fetch_many(sqlx::query(sql) $( .bind($param) )*)
+                            .try_filter_map(|step| async move {
+                                Ok(match step {
+                                    Either::Left(_) => None,
+                                    Either::Right(row) => M::from_row(&row).ok(),
+                                })
+                            })
+                    )
+                    .try_collect()
+                )
+            }
+        }
+    };
+} const _: () = {
+    impl_q_fetch_all_with_params!();
+    impl_q_fetch_all_with_params!(p1);
+    impl_q_fetch_all_with_params!(p1 p2);
+    impl_q_fetch_all_with_params!(p1 p2 p3);
+    impl_q_fetch_all_with_params!(p1 p2 p3 p4);
+    impl_q_fetch_all_with_params!(p1 p2 p3 p4 p5);
+    impl_q_fetch_all_with_params!(p1 p2 p3 p4 p5 p6);
+    impl_q_fetch_all_with_params!(p1 p2 p3 p4 p5 p6 p7);
+};
 
+macro_rules! impl_q_fetch_one_with_params {
+    ($( $param:ident )*) => {
+        #[allow(non_camel_case_types)]
+        impl<'q, M:Model+'q, $( $param:Param<'q> ),*> FnOnce<(FetchOne<'q, M>, $( $param ),*)> for q {
+            type Output = Pin<Box<dyn Future<Output = Result<Vec<M>, Error>> + 'q>>;
+            extern "rust-call" fn call_once(self,
+                (FetchOne{sql, __as__}, $( $param ),*): (FetchOne<'q, M>, $( $param ),*)
+            ) -> Self::Output {
+                Box::pin(
+                    Box::pin(
+                        pool().fetch_many(sqlx::query(sql) $( .bind($param) )*)
+                            .try_filter_map(|step| async move {
+                                Ok(match step {
+                                    Either::Left(_) => None,
+                                    Either::Right(row) => M::from_row(&row).ok(),
+                                })
+                            })
+                    )
+                    .try_collect()
+                )
+            }
+        }
+    };
+} const _: () = {
+    impl_q_fetch_one_with_params!();
+    impl_q_fetch_one_with_params!(p1);
+    impl_q_fetch_one_with_params!(p1 p2);
+    impl_q_fetch_one_with_params!(p1 p2 p3);
+    impl_q_fetch_one_with_params!(p1 p2 p3 p4);
+    impl_q_fetch_one_with_params!(p1 p2 p3 p4 p5);
+    impl_q_fetch_one_with_params!(p1 p2 p3 p4 p5 p6);
+    impl_q_fetch_one_with_params!(p1 p2 p3 p4 p5 p6 p7);
+};
 
-    /// Execute the query and return the generated results as a stream.
-    fn fetch<'e, 'q: 'e, E: 'q>(
-        self,
-        query: E,
-    ) -> BoxStream<'e, Result<<Self::Database as Database>::Row, Error>>
-    where
-        'c: 'e,
-        E: Execute<'q, Self::Database>,
-    {
-        self.fetch_many(query)
-            .try_filter_map(|step| async move {
-                Ok(match step {
-                    Either::Left(_) => None,
-                    Either::Right(row) => Some(row),
-                })
-            })
-            .boxed()
-    }
-
-    /// Execute the query and return all the generated results, collected into a [`Vec`].
-    fn fetch_all<'e, 'q: 'e, E: 'q>(
-        self,
-        query: E,
-    ) -> BoxFuture<'e, Result<Vec<<Self::Database as Database>::Row>, Error>>
-    where
-        'c: 'e,
-        E: Execute<'q, Self::Database>,
-    {
-        self.fetch(query).try_collect().boxed()
-    }
-
-*/
-
-impl<'q, M:Model+'q> FnOnce<(FetchAll<'q, M>,)> for q {
-    type Output = Pin<Box<dyn Future<Output = Result<Vec<M>, Error>> + 'q>>;
-    extern "rust-call" fn call_once(self,
-        (FetchAll{sql, __as__},): (FetchAll<'q, M>,)
-    ) -> Self::Output {
-        Box::pin(
-            Box::pin(
-                pool().fetch_many(sqlx::query(sql))
-                    .try_filter_map(|step| async move {
-                        Ok(match step {
-                            Either::Left(_) => None,
-                            Either::Right(row) => M::from_row(&row).ok(),
-                        })
-                    })
-            )
-            .try_collect()
-        )
-    }
-}
+macro_rules! impl_q_fetch_optional_with_params {
+    ($( $param:ident )*) => {
+        #[allow(non_camel_case_types)]
+        impl<'q, M:Model+'q, $( $param:Param<'q> ),*> FnOnce<(FetchOptional<'q, M>, $( $param ),*)> for q {
+            type Output = Pin<Box<dyn Future<Output = Result<Vec<M>, Error>> + 'q>>;
+            extern "rust-call" fn call_once(self,
+                (FetchOptional{sql, __as__}, $( $param ),*): (FetchOptional<'q, M>, $( $param ),*)
+            ) -> Self::Output {
+                Box::pin(
+                    Box::pin(
+                        pool().fetch_many(sqlx::query(sql) $( .bind($param) )*)
+                            .try_filter_map(|step| async move {
+                                Ok(match step {
+                                    Either::Left(_) => None,
+                                    Either::Right(row) => M::from_row(&row).ok(),
+                                })
+                            })
+                    )
+                    .try_collect()
+                )
+            }
+        }
+    };
+} const _: () = {
+    impl_q_fetch_optional_with_params!();
+    impl_q_fetch_optional_with_params!(p1);
+    impl_q_fetch_optional_with_params!(p1 p2);
+    impl_q_fetch_optional_with_params!(p1 p2 p3);
+    impl_q_fetch_optional_with_params!(p1 p2 p3 p4);
+    impl_q_fetch_optional_with_params!(p1 p2 p3 p4 p5);
+    impl_q_fetch_optional_with_params!(p1 p2 p3 p4 p5 p6);
+    impl_q_fetch_optional_with_params!(p1 p2 p3 p4 p5 p6 p7);
+};
