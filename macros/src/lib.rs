b8193a8cff7897efc5869ef6f2d9c844ee09505d
kana-rus <kanarus786@gmail.com>
2023-05-14 20:49:48 +0900

    save@2023-05-14 20:49+9:00

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,1 +1,9 @@
+use proc_macro::TokenStream;
 
+mod model;
+#[proc_macro]
+pub fn model(input: TokenStream) -> TokenStream {
+    model::model(input.into())
+        .unwrap_or_else(|e| e.into_compile_error())
+        .into()
+}
---
+++ macros/src/model/mod.rs
@@ -0,0 +1,6 @@
+use proc_macro2::TokenStream;
+use syn::Error;
+
+pub(super) fn model(input: TokenStream) -> Result<TokenStream, Error> {
+    todo!()
+}
