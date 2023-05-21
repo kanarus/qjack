5d6b59a748bbfc6f7f0acc94f10ecbb859d44648
kana-rus <kanarus786@gmail.com>
2023-05-21 22:58:09 +0900

    Add; `Model` impls

--- publish.sh
+++ publish.sh
@@ -1,1 +1,2 @@
+cargo check --features rt_tokio,db_postgres
 cargo publish --no-verify
--- src/model.rs
+++ src/model.rs
@@ -25,3 +25,83 @@ pub trait Model: for<'r> FromRow<'r, __feature__::Row> {
         FetchOptional { sql, __as__:PhantomData }
     }
 }
+
+/*
+macro_rules! impl_from_row_for_tuple {
+    ($( ($idx:tt) -> $T:ident );+;) => {
+        impl<'r, R, $($T,)+> FromRow<'r, R> for ($($T,)+)
+        where
+            R: Row,
+            usize: crate::column::ColumnIndex<R>,
+            $($T: crate::decode::Decode<'r, R::Database> + crate::types::Type<R::Database>,)+
+        {
+            #[inline]
+            fn from_row(row: &'r R) -> Result<Self, Error> {
+                Ok(($(row.try_get($idx as usize)?,)+))
+            }
+        }
+    };
+}
+*/
+
+macro_rules! impl_model_for_tuple {
+    ( $( $index:literal -> $T:ident; )+ ) => {
+        impl<$( $T ),+> Model for ( $( $T, )+ )
+        where
+            $($T: for<'r> crate::__private__::Decode<'r, __feature__::DB> + crate::__private__::Type<__feature__::DB>,)+
+        {}
+    };
+} const _: () = {
+    impl_model_for_tuple!(
+        0 -> T0;
+    );
+    impl_model_for_tuple!(
+        0 -> T0;
+        1 -> T1;
+    );
+    impl_model_for_tuple!(
+        0 -> T0;
+        1 -> T1;
+        2 -> T2;
+    );
+    impl_model_for_tuple!(
+        0 -> T0;
+        1 -> T1;
+        2 -> T2;
+        3 -> T3;
+    );
+    impl_model_for_tuple!(
+        0 -> T0;
+        1 -> T1;
+        2 -> T2;
+        3 -> T3;
+        4 -> T4;
+    );
+    impl_model_for_tuple!(
+        0 -> T0;
+        1 -> T1;
+        2 -> T2;
+        3 -> T3;
+        4 -> T4;
+        5 -> T5;
+    );
+    impl_model_for_tuple!(
+        0 -> T0;
+        1 -> T1;
+        2 -> T2;
+        3 -> T3;
+        4 -> T4;
+        5 -> T5;
+        6 -> T6;
+    );
+    impl_model_for_tuple!(
+        0 -> T0;
+        1 -> T1;
+        2 -> T2;
+        3 -> T3;
+        4 -> T4;
+        5 -> T5;
+        6 -> T6;
+        7 -> T7;
+    );
+};
